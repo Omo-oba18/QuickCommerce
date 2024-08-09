3003a331b904f81d5eb749029956a34d8270053c
Chablis Mahutin <omoobaoshoffa@gmail.com>
2024-08-09 13:08:42 +0100

    removed the last conflict

--- Models/User.cs
+++ Models/User.cs
@@ -12,13 +12,13 @@ namespace QuickCommerce.Models
             get; set;
         }
 
-        public string Username { get; set; }
+        public string Username { get; set; } = string.Empty;
         [Required]
         [EmailAddress]
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
-        public string PhoneNumber { get; set; }
-        public string ProfilePicture { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string ProfilePicture { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
